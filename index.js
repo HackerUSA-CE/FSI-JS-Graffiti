66a8ab656d34fd193d6ed352fbd58bb9cd8cfdee
Joshua Miles <joshua.m@hackerusa.com>
2020-09-22 11:32:26 -0500

    _

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
 
-// Select some elements...
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
