dd932bfe3fcba44c80aebdbd703dac51e16448c1
Tzvetomir Kotzev <tzvetomirkotzev@Tzvetomirs-MacBook-Air.local>
2022-11-15 18:11:31 -0500

    Doggos dog

--- index.js
+++ index.js
@@ -9,5 +9,15 @@ for(let i = 0; i < dogImages.length; i++){
 	dogImages[i].style.borderRadius = '50px'
 }
 
-//footer.style.text
+//footer.style.borderStyle = 'solid'
+let footer = document.querySelector('.footer')
+footer.style.color = 'purple'
+footer.style.borderStyle = 'solid'
+footer.style.backgroundColor = 'beige'
 
+let doggoNames = document.querySelectorAll('.dog-name')
+for (let i = 0; i < doggoNames.length; i++){
+	doggoNames[i].style.textAlign = 'left'
+	doggoNames[i].style.color = 'navy'
+	doggoNames[i].style.font = 'Arial'
+}
