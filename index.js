2c87f76a8e5fd0d28d6c528f095bebc379899357
Tzvetomir Kotzev <tzvetomirkotzev@Tzvetomirs-MacBook-Air.local>
2022-11-14 20:04:48 -0500

    my graffiti

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Select some elements...
+
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
+footer.style.text
+
